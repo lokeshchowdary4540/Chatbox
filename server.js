e638fd31ab5de27070259a08777b935361242c6d
Lokesh <lokeshchowdary4540@gmail.com>
2026-08-25 17:27:25 +0530

    update

--- server.js
+++ server.js
@@ -1,21 +1,87 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({
-    port: 8080
+
+// ================================
+// HTTP SERVER
+// ================================
+
+const server = http.createServer((req, res) => {
+
+    const filePath = path.join(__dirname, "public", "index.html");
+
+    fs.readFile(filePath, (error, data) => {
+
+        if (error) {
+            res.writeHead(500);
+            res.end("Could not load index.html");
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+
+        res.end(data);
+    });
+
 });
 
-server.on("connection", (socket) => {
-    console.log("A user connected!");
 
+// ================================
+// WEBSOCKET SERVER
+// ================================
+
+const wss = new WebSocket.Server({
+    server: server
+});
+
+
+wss.on("connection", (socket) => {
+
+    console.log("A user connected");
+
+    // Send welcome message
     socket.send("Welcome to the group chat!");
 
+
+    // Receive message
     socket.on("message", (message) => {
-        console.log("User says:", message.toString());
+
+    console.log("User says:", message.toString());
+
+    wss.clients.forEach((client) => {
+
+        if (client.readyState === WebSocket.OPEN) {
+
+            client.send(message.toString());
+
+        }
+
     });
 
-    socket.on("close", () => {
-        console.log("A user disconnected.");
-    });
 });
 
-console.log("WebSocket server running at ws://localhost:8080");
+
+    // User disconnects
+    socket.on("close", () => {
+
+        console.log("A user disconnected");
+
+    });
+
+});
+
+
+// ================================
+// START SERVER
+// ================================
+
+server.listen(8080, () => {
+
+    console.log("Server running at:");
+    console.log("http://localhost:8080");
+
+});
